f6c7c6d0c003977aad3358e36387ed405585ba4d
SJ9211 <jsj921127@naver.com>
2025-01-31 14:41:37 +0900

    Invoke

---
+++ Assets/Scripts/InvokeScript.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class InvokeScript : MonoBehaviour
+{
+   enum State {make,stop};
+   State state = State.make;
+   
+   public GameObject target;
+    void Start()
+    {
+        InvokeRepeating ("SpawnObject", 2, 1); 
+        //state = State.make;
+    }
+
+     void Update()
+     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (state == State.make)
+            {
+                CancelInvoke("SpawnObject");
+                state = State.stop;
+            }
+            else if (state == State.stop)
+            {
+                InvokeRepeating ("SpawnObject", 2, 1);
+                state =State.make;
+            }
+            
+        }
+     }
+
+    void SpawnObject()
+    {
+        int a = Random.Range(1, 10);
+        
+        float x = Random.Range(-2.0f, 2.0f);
+
+        float z = Random.Range(-2.0f, 2.0f);
+        Instantiate(target, new Vector3(z, 4, x), Quaternion.identity);
+    }
+}
